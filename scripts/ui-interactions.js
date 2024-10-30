5e66ba754c3fdc9e59a26ab80e7f9261dc15e829
Melchard <melchardsimeon71@gmail.com>
2024-10-30 16:37:12 +0800

    fix sidebar delay when openr delay when opening

--- scripts/ui-interactions.js
+++ scripts/ui-interactions.js
@@ -5,33 +5,25 @@ const openSidebar = document.querySelector('.open-sidebar');
 const sidebarMenu = document.querySelector('.sidebar-menu');
 const closeSidebar = document.querySelector('#close-sidebar')
 const overlay = document.querySelector('.overlay')
+const isSidebarOpen = getComputedStyle(sidebarMenu).transform === 'none'; // Change this as per your needs
+
 
 // Function to open the sidebar
 
 
 openSidebar.addEventListener('click', () => {
-    if (sidebarMenu.style.transform === 'translateX(-100%)') {
-
+    sidebarMenu.classList.toggle('active');
+    if (sidebarMenu.classList.contains('active')) {
         sidebarMenu.style.transform = 'translateX(0)';
         sidebarMenu.style.opacity = '1';
-        
         overlay.style.display = 'block';
-        setTimeout(() => {
-            overlay.style.opacity = '1'
-        }, 10);
-
-        document.body.style.overflow = 'hidden'
     } else {
         sidebarMenu.style.transform = 'translateX(-100%)';
         sidebarMenu.style.opacity = '0';
-
         overlay.style.display = 'none';
+    }
+});
 
-        setTimeout(() => {
-            overlay.style.opacity = '0';
-            document.body.style.overflow = '';
-        }, 300)}
-})
 
 closeSidebar.addEventListener('click', () => {
     sidebarMenu.style.transform = 'translateX(-100%)'
